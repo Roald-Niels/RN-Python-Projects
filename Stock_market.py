387be2047c4ae0262d2c2d7ce371ee988a8725a6
iOdenn <78323364+iOdenn@users.noreply.github.com>
2022-07-31 15:17:38 -0400

    Create Stock_market.py

---
+++ Stock_market.py
@@ -0,0 +1,16 @@
+stocks = {
+'DIS':118.27,
+'WBA': 45.54,
+'NFLX': 215.52, 
+'ATVI': 78.61, 
+'SONY': 85.60, 
+'CVS': 101.68, 
+'NTDOF': 497, 
+'TSLA': 1005.05, 
+'WMT': 156.86,
+'STAG': 40.77
+}
+
+entry = input("Type ticker symbol: ")
+if entry in stocks:
+    print ("Ticker symbol: " + entry)
